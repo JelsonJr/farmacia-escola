991f8425036742cb9e9d3d3a803d66f805762d03
JelsonJr <jelson.rodrigues.junior@gmail.com>
2023-10-30 14:14:37 -0300

    first commit

---
+++ src/main/java/br/com/uepg/farmaciaescola/FarmaciaEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.uepg.farmaciaescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaEscolaApplication.class, args);
+	}
+
+}
